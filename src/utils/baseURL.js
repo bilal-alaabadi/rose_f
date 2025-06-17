21ee82924d588c382774b9ece844e709e9ecc63a
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-06-17 18:24:55 +0400

    تحسينات على صفحة المتجر لجعلها متجاوبة مع الهواتف وإصلاح مشكلة ثبات المنتجات

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,5 +2,5 @@
 //     return 'https://pinkheart-b.vercel.app';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+    return 'https://glassy-b-w5bs.vercel.app';//رابط الباك اند بدل 5000
 }
